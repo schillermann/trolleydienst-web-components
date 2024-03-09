496c6fc2c022851887e2ebbc8f33fed570534883
Mario Schillermann <mario.schillermann@gmail.com>
2024-03-09 14:59:19 +0100

    change api call for shift calendar

--- public/js/components/shift-calender.js
+++ public/js/components/shift-calender.js
@@ -1,5 +1,4 @@
-import { LitElement, html } from "../lit-core.min.js";
-import { Task } from "https://cdn.jsdelivr.net/npm/@lit/task@1.0.0/+esm";
+import { LitElement, until, html } from "../lit-all.min.js";
 import "./shift/shift-route.js";
 
 export class ShiftCalendar extends LitElement {
@@ -11,35 +10,23 @@ export class ShiftCalendar extends LitElement {
     super();
   }
 
-  _routesTask = new Task(this, {
-    task: async ([calendarId], { signal }) => {
-      const response = await fetch(`/api/calendars/${calendarId}/routes.json`, {
-        signal,
-      });
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    },
-    args: () => [this.calendarId],
-  });
-
   render() {
-    return this._routesTask.render({
-      pending: () => html`<p>Loading routes...</p>`,
-      complete: (routes) =>
+    const routes = fetch(`/api/calendars/${this.calendarId}/routes.json`).then(
+      (response) => response.json()
+    );
+    return html`${until(
+      routes.then((routes) =>
         routes.map(
-          (route) => html`
-            <shift-route
-              currentPublisherId="1"
-              routeName="${route.routeName}"
-              shiftStart="${new Date().toISOString()}"
-              shifts="${JSON.stringify(route.shifts)}"
-            ></shift-route>
-          `
-        ),
-      error: (e) => html`<p>Error: ${e}</p>`,
-    });
+          (route) => html` <shift-route
+            currentPublisherId="1"
+            routeName="${route.routeName}"
+            shiftStart="${new Date().toISOString()}"
+            shifts="${JSON.stringify(route.shifts)}"
+          ></shift-route>`
+        )
+      ),
+      html`<span>Loading...</span>`
+    )}`;
   }
 }
 customElements.define("shift-calendar", ShiftCalendar);
